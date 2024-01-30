f03ba878dfd9f078816533c6c36d8acb6f21ee2c
Chia-Wei <52095472+open-minded13@users.noreply.github.com>
2024-01-30 10:11:17 -0800

    new file:   under_construction/web_app_full_stack/package-lock.json 	new file:   under_construction/web_app_full_stack/screenshot.png 	new file:   under_construction/web_app_full_stack/trailx/.eslintrc.cjs 	new file:   under_construction/web_app_full_stack/trailx/.gitignore 	new file:   under_construction/web_app_full_stack/trailx/.npmrc 	new file:   under_construction/web_app_full_stack/trailx/Dockerfile 	new file:   under_construction/web_app_full_stack/trailx/README.MD 	new file:   under_construction/web_app_full_stack/trailx/index.html 	new file:   under_construction/web_app_full_stack/trailx/package-lock.json 	new file:   under_construction/web_app_full_stack/trailx/package.json 	new file:   under_construction/web_app_full_stack/trailx/public/favicon.ico 	new file:   under_construction/web_app_full_stack/trailx/src/App.tsx 	new file:   under_construction/web_app_full_stack/trailx/src/index.tsx 	new file:   under_construction/web_app_full_stack/trailx/src/pages/forgotPassword/index.tsx 	new file:   under_construction/web_app_full_stack/trailx/src/pages/login/index.tsx 	new file:   under_construction/web_app_full_stack/trailx/src/pages/register/index.tsx 	new file:   under_construction/web_app_full_stack/trailx/src/providers/auth.ts 	new file:   under_construction/web_app_full_stack/trailx/src/providers/data/fetch-wrapper.ts 	new file:   under_construction/web_app_full_stack/trailx/src/providers/data/index.tsx 	new file:   under_construction/web_app_full_stack/trailx/src/providers/index.ts 	new file:   under_construction/web_app_full_stack/trailx/src/vite-env.d.ts 	new file:   under_construction/web_app_full_stack/trailx/tsconfig.json 	new file:   under_construction/web_app_full_stack/trailx/tsconfig.node.json 	new file:   under_construction/web_app_full_stack/trailx/vite.config.ts

---
+++ under_construction/web_app_full_stack/trailx/src/App.tsx
@@ -0,0 +1,54 @@
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { useNotificationProvider } from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import { dataProvider, liveProvider, authProvider } from "./providers";
+import routerBindings, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import { App as AntdApp } from "antd";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <AntdApp>
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              liveProvider={liveProvider}
+              notificationProvider={useNotificationProvider}
+              routerProvider={routerBindings}
+              authProvider={authProvider}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                useNewQueryKeys: true,
+                projectId: "47T19D-4nzciy-pcv3Ki",
+                liveMode: "auto",
+              }}
+            >
+              <Routes>
+                <Route index element={<WelcomePage />} />
+                <Route />
+              </Routes>
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
+        </AntdApp>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
