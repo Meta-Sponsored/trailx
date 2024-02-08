7fa0e9dd77562d2a5da8202676cd7d43c2812565
leunghon <leunghon5@gmail.com>
2024-02-07 22:42:27 -0800

    gitignore

--- under_construction/web_app_frontend/src/firebase-config.js
+++ under_construction/web_app_frontend/src/firebase-config.js
@@ -8,13 +8,13 @@ import { getFirestore, collection, getDocs, serverTimestamp } from 'firebase/fir
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCCHWikzOvBvmWS39pzrI0O3luNIfvmuMA",
-  authDomain: "gix-trailx.firebaseapp.com",
-  projectId: "gix-trailx",
-  storageBucket: "gix-trailx.appspot.com",
-  messagingSenderId: "323140771813",
-  appId: "1:323140771813:web:72000aae9b2bf96c25590c",
-  measurementId: "G-D9D0JRVVV6"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
